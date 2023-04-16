e9e357e70846ebb7154705d91c9338efbd9387b6
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2023-04-16 18:36:59 +0300

    Implement OrderAPI to send "New Order" messages requested through WebApi.

---
+++ OrderAPI/OrderService.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Text.Json;
+using Common;
+using RabbitMQ.Client;
+
+namespace OrderAPI
+{
+    public class OrderService : IOrderService
+    {
+        public OrderService(ILogger<OrderService> logger)
+        {
+            Logger = logger;
+
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            Connection = factory.CreateConnection();
+            Channel = Connection.CreateModel();
+
+            Channel.QueueDeclare(queue: Consts.NewOrderQueue,
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+        }
+
+        public Task CreateOrder(Order order)
+        {
+            var message = $"Order \'{JsonSerializer.Serialize(order)}\' requested";
+            var body = Encoding.UTF8.GetBytes(message);
+
+            Channel.BasicPublish(exchange: string.Empty,
+                                 routingKey: Consts.NewOrderQueue,
+                                 basicProperties: null,
+                                 body: body);
+
+            Logger.LogInformation($"[x] Sent {message}");
+
+            return Task.CompletedTask;
+        }
+
+        private readonly ILogger<OrderService> Logger;
+        private readonly IConnection Connection;
+        private readonly IModel Channel; 
+    }
+}
--- OrderAPI/Program.cs
+++ OrderAPI/Program.cs
@@ -1,6 +1,9 @@
+using OrderAPI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddSingleton<IOrderService, OrderService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
