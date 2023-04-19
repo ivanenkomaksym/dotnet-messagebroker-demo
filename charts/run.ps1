14b865976db2dd885d8e28a3fd4e86cdc7d0d686
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2023-04-19 13:10:59 +0300

    Add config maps with k8s-specific appsettings. Remove service deployment for notifications, orderprocessor and warehouse. Update run.ps1 and cleanup.ps1.

--- charts/run.ps1
+++ charts/run.ps1
@@ -1,15 +1,14 @@
-minikube start
-minikube docker-env
-minikube -p minikube docker-env --shell powershell | Invoke-Expression
-
+kubectl apply -f notifications_configmap.yaml
 kubectl apply -f notifications.yaml
+kubectl apply -f orderapi_configmap.yaml
 kubectl apply -f orderapi.yaml
+kubectl apply -f orderprocessor_configmap.yaml
 kubectl apply -f orderprocessor.yaml
+kubectl apply -f warehouse_configmap.yaml
 kubectl apply -f warehouse.yaml
 
-kubectl apply -f notifications_service.yaml
+kubectl apply -f rabbitmq.yaml
+
 kubectl apply -f orderapi_service.yaml
-kubectl apply -f orderprocessor_service.yaml
-kubectl apply -f warehouse_service.yaml
 
 minikube service orderapi-service
