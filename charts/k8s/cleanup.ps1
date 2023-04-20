fd82cd96827165e06db621d68514d6f5a71de92c
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2023-04-20 15:23:20 +0300

    Update k8s yaml files and run_k8s.ps1 script.

--- charts/k8s/cleanup.ps1
+++ charts/k8s/cleanup.ps1
@@ -7,4 +7,7 @@ kubectl delete -f orderprocessor.yaml
 kubectl delete -f warehouse_configmap.yaml
 kubectl delete -f warehouse.yaml
 
+kubectl delete -f rabbitmq.yaml
+kubectl delete -f rabbitmq_service.yaml
+
 kubectl delete -f orderapi_service.yaml
--- run_k8s.ps1
+++ run_k8s.ps1
@@ -1,3 +1,6 @@
+cd charts\k8s
+.\cleanup.ps1
+
 kubectl apply -f notifications_configmap.yaml
 kubectl apply -f notifications.yaml
 kubectl apply -f orderapi_configmap.yaml
@@ -8,7 +11,9 @@ kubectl apply -f warehouse_configmap.yaml
 kubectl apply -f warehouse.yaml
 
 kubectl apply -f rabbitmq.yaml
+kubectl apply -f rabbitmq_service.yaml
 
 kubectl apply -f orderapi_service.yaml
 
+minikube dashboard
 minikube service orderapi-service
