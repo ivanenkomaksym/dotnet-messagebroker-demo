04d871959955104d1e845ae2f89caf64be158acf
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2023-04-22 16:30:00 +0300

    Get rid of individual config maps and use shared app_configuration instead. Set replicas to 2 for notifications and warehouse microservices.

--- charts/k8s/cleanup.ps1
+++ charts/k8s/cleanup.ps1
@@ -1,10 +1,8 @@
-kubectl delete -f notifications_configmap.yaml
+kubectl delete -f app_configmap.yaml
+
 kubectl delete -f notifications.yaml
-kubectl delete -f orderapi_configmap.yaml
 kubectl delete -f orderapi.yaml
-kubectl delete -f orderprocessor_configmap.yaml
 kubectl delete -f orderprocessor.yaml
-kubectl delete -f warehouse_configmap.yaml
 kubectl delete -f warehouse.yaml
 
 kubectl delete -f rabbitmq.yaml
