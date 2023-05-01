94e7da159e2b44f4fec82fad256b313ea4440759
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2023-05-01 17:25:58 +0300

    Add OcelotAPIGateway helm chart. Update helm charts to get from Values.service  * containerPort for deployment.yaml  * port and targetPort for service.yaml

--- 05_run_helm.ps1
+++ 05_run_helm.ps1
@@ -2,4 +2,5 @@ helm upgrade --install helmapp .\charts\helmapp
 minikube dashboard
 Start-Process -FilePath "cmd" -ArgumentList "/c minikube service helmapp-orderapi" -WorkingDirectory ".\"
 Start-Process -FilePath "cmd" -ArgumentList "/c minikube service helmapp-customerapi" -WorkingDirectory ".\"
+Start-Process -FilePath "cmd" -ArgumentList "/c minikube service helmapp-ocelotapigateway" -WorkingDirectory ".\"
 Start-Process -FilePath "cmd" -ArgumentList "/c minikube service helmapp-rabbitmq" -WorkingDirectory ".\"
