c904fa77ee7058c7f93b8556a5a3c213b9b360ce
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2023-05-07 18:05:41 +0300

    Add basic UI for Register and Sing In buttons.

---
+++ WebUI/Pages/Customers/Create.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Common.Models;
+using WebUI.Services;
+
+namespace WebUI.Pages.Customers
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ICustomerService _customerService;
+
+        public CreateModel(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Customer Customer { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            await _customerService.CreateCustomer(Customer);
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
