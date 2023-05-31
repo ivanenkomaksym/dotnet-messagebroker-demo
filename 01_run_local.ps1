56d1d0f7eafe141b8fbccdf2f57b776078d52559
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2023-05-31 20:28:12 +0300

    Update docker configuration w.r.t. new OrderGrpc. Update Ocelot configuration and launchSettings.json of the rest microservices.

--- 01_run_local.ps1
+++ 01_run_local.ps1
@@ -1,4 +1,5 @@
 Start-Process -FilePath "cmd" -ArgumentList "/c docker run -it --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.11-management" -WorkingDirectory ".\"
+Start-Process -FilePath "cmd" -ArgumentList "/c docker run -it --rm --name mongo -p 27017:27017 mongo:latest" -WorkingDirectory ".\"
 Start-Sleep -Seconds 15
 Start-Process -FilePath "cmd" -ArgumentList "/c dotnet run" -WorkingDirectory ".\Notifications"
 Start-Process -FilePath "cmd" -ArgumentList "/c dotnet run" -WorkingDirectory ".\OrderAPI"
