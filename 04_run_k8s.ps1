f16321457f7414992755621cabf638ef73d54e89
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2023-06-12 19:39:50 +0300

    Update k8s support.

--- 04_run_k8s.ps1
+++ 04_run_k8s.ps1
@@ -1,25 +1,68 @@
 cd charts\k8s
 .\cleanup.ps1
 
-kubectl apply -f app_configmap.yaml
-kubectl apply -f ocelot_configmap.yaml
-kubectl apply -f notifications.yaml
-kubectl apply -f orderapi.yaml
-kubectl apply -f orderprocessor.yaml
-kubectl apply -f warehouse.yaml
-kubectl apply -f customerapi.yaml
-kubectl apply -f ocelotapigateway.yaml
-
+# RabbitMQ
 kubectl apply -f rabbitmq.yaml
-kubectl apply -f rabbitmq_service.yaml
+kubectl apply -f rabbitmq-service.yaml
 
-kubectl apply -f orderapi_service.yaml
-kubectl apply -f customerapi_service.yaml
-kubectl apply -f ocelotapigateway_service.yaml
+# Mongo
+kubectl apply -f mongo-secret.yaml
+kubectl apply -f mongo-deployment.yaml
+kubectl apply -f mongo-service.yaml
+
+# CatalogAPI
+kubectl apply -f catalogapi.yaml
+kubectl apply -f catalogapi-service.yaml
+
+# CustomerAPI
+kubectl apply -f customerapi.yaml
+kubectl apply -f customerapi-service.yaml
+
+# Notifications
+kubectl apply -f notifications.yaml
+
+# OcelotApiGateway
+kubectl apply -f ocelot-configmap.yaml
+kubectl apply -f ocelotapigateway.yaml
+kubectl apply -f ocelotapigateway-service.yaml
+
+# OrderAPI
+kubectl apply -f orderapi.yaml
+kubectl apply -f orderapi-service.yaml
+
+# OrderGRPC
+kubectl apply -f ordergrpc.yaml
+kubectl apply -f ordergrpc-service.yaml
+
+# OrderProcessor
+kubectl apply -f orderprocessor.yaml
+
+# Payment
+kubectl apply -f payment.yaml
+
+# Shipment
+kubectl apply -f shipment.yaml
+
+# ShoppingCartAPI
+kubectl apply -f shoppingcartapi.yaml
+kubectl apply -f shoppingcartapi-service.yaml
+
+# Warehouse
+kubectl apply -f warehouse.yaml
+
+# WarehouseAPI
+kubectl apply -f warehouseapi.yaml
+kubectl apply -f warehouseapi-service.yaml
+
+# WebUI
+kubectl apply -f webui.yaml
+kubectl apply -f webui-service.yaml
+
+# WebUIAggregatorAPI
+kubectl apply -f webuiaggregatorapi.yaml
+kubectl apply -f webuiaggregatorapi-service.yaml
 
 minikube dashboard
-Start-Process -FilePath "cmd" -ArgumentList "/c minikube service orderapi-service" -WorkingDirectory ".\"
-Start-Process -FilePath "cmd" -ArgumentList "/c minikube service customerapi-service" -WorkingDirectory ".\"
-Start-Process -FilePath "cmd" -ArgumentList "/c minikube service ocelotapigateway-service" -WorkingDirectory ".\"
-Start-Process -FilePath "cmd" -ArgumentList "/c minikube service rabbitmq-service" -WorkingDirectory ".\"
+Start-Process -FilePath "cmd" -ArgumentList "/c minikube service shopdb" -WorkingDirectory ".\"
+Start-Process -FilePath "cmd" -ArgumentList "/c minikube service webui" -WorkingDirectory ".\"
 cd ..\..\
--- charts/k8s/cleanup.ps1
+++ charts/k8s/cleanup.ps1
@@ -1,16 +1,60 @@
-kubectl delete -f app_configmap.yaml
-kubectl delete -f ocelot_configmap.yaml
-
-kubectl delete -f notifications.yaml
-kubectl delete -f orderapi.yaml
-kubectl delete -f orderprocessor.yaml
-kubectl delete -f warehouse.yaml
-kubectl delete -f customerapi.yaml
-kubectl delete -f ocelotapigateway.yaml
-
+# RabbitMQ
 kubectl delete -f rabbitmq.yaml
-kubectl delete -f rabbitmq_service.yaml
+kubectl delete -f rabbitmq-service.yaml
 
-kubectl delete -f orderapi_service.yaml
-kubectl delete -f customerapi_service.yaml
-kubectl delete -f ocelotapigateway_service.yaml
+# Mongo
+kubectl delete -f mongo-secret.yaml
+kubectl delete -f mongo-deployment.yaml
+kubectl delete -f mongo-service.yaml
+
+# CatalogAPI
+kubectl delete -f catalogapi.yaml
+kubectl delete -f catalogapi-service.yaml
+
+# CustomerAPI
+kubectl delete -f customerapi.yaml
+kubectl delete -f customerapi-service.yaml
+
+# Notifications
+kubectl delete -f notifications.yaml
+
+# OcelotApiGateway
+kubectl delete -f ocelot-configmap.yaml
+kubectl delete -f ocelotapigateway.yaml
+kubectl delete -f ocelotapigateway-service.yaml
+
+# OrderAPI
+kubectl delete -f orderapi.yaml
+kubectl delete -f orderapi-service.yaml
+
+# OrderGRPC
+kubectl delete -f ordergrpc.yaml
+kubectl delete -f ordergrpc-service.yaml
+
+# OrderProcessor
+kubectl delete -f orderprocessor.yaml
+
+# Payment
+kubectl delete -f payment.yaml
+
+# Shipment
+kubectl delete -f shipment.yaml
+
+# ShoppingCartAPI
+kubectl delete -f shoppingcartapi.yaml
+kubectl delete -f shoppingcartapi-service.yaml
+
+# Warehouse
+kubectl delete -f warehouse.yaml
+
+# WarehouseAPI
+kubectl delete -f warehouseapi.yaml
+kubectl delete -f warehouseapi-service.yaml
+
+# WebUI
+kubectl delete -f webui.yaml
+kubectl delete -f webui-service.yaml
+
+# WebUIAggregatorAPI
+kubectl delete -f webuiaggregatorapi.yaml
+kubectl delete -f webuiaggregatorapi-service.yaml
