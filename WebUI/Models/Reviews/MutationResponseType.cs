ee99f0d8ad1aec71a4b91c9c9aef58e01744f62a
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2023-08-06 11:59:18 +0300

    Implement GraphQL FeedbackService.PostReview.

---
+++ WebUI/Models/Reviews/MutationResponseType.cs
@@ -0,0 +1,12 @@
+﻿using Common.Models.Review;
+
+namespace WebUI.Models.Reviews
+{
+    public class MutationResponseType
+    {
+        /// <summary>
+        /// Name of this property must match with the one returned from GraphQL endpoint
+        /// </summary>
+        public Review Review { get; set; }
+    }
+}
--- WebUI/Services/FeedbackService.cs
+++ WebUI/Services/FeedbackService.cs
@@ -45,9 +45,23 @@ namespace WebUI.Services
             return response.Data.ReviewsByProductId;
         }
 
-        public Task<Review> PostReview(Guid productId, Review review)
+        public async Task<Review> PostReview(Guid productId, Review review)
         {
-            throw new NotImplementedException();
+            var query = new GraphQLRequest
+            {
+                Query = @"
+                mutation reviewForProductId($productId: UUID!, $review: ReviewInput!){
+                  postReviewForProductId(productId: $productId, review: $review){
+                    id,
+                    description,
+                    rating,
+                    creationDateTime
+                  }
+                }",
+                Variables = new { productId = productId, review = review }
+            };
+            var response = await _client.SendMutationAsync<MutationResponseType>(query);
+            return response.Data.Review;
         }
     }
 }
