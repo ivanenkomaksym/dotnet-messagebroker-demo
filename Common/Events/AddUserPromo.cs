8b8c8b6c3231c3b2e07b1d0d16b9cd58023a9832
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2023-08-19 16:42:06 +0300

    Make OrderProcessor send new AddUserPromo event when order compeleted

---
+++ Common/Events/AddUserPromo.cs
@@ -0,0 +1,13 @@
+﻿using Common.Models;
+
+namespace Common.Events
+{
+    public record AddUserPromo
+    {
+        public CustomerInfo CustomerInfo { get; init; }
+
+        public double Promo { get; init; }
+
+        public DateTime ValidUntil { get; init; }
+    }
+}
--- OrderProcessor/Consumers/OrderCollectedConsumer.cs
+++ OrderProcessor/Consumers/OrderCollectedConsumer.cs
@@ -8,11 +8,13 @@ namespace OrderProcessor.Consumers
     internal class OrderCollectedConsumer : IConsumer<OrderCollected>
     {
         private readonly IGrpcOrderClient _grpcOrderClient;
+        private readonly IPublishEndpoint _publishEndpoint;
         private readonly ILogger<OrderCollectedConsumer> _logger;
 
-        public OrderCollectedConsumer(IGrpcOrderClient grpcOrderClient, ILogger<OrderCollectedConsumer> logger)
+        public OrderCollectedConsumer(IGrpcOrderClient grpcOrderClient, IPublishEndpoint publishEndpoint , ILogger<OrderCollectedConsumer> logger)
         {
             _grpcOrderClient = grpcOrderClient;
+            _publishEndpoint = publishEndpoint;
             _logger = logger;
         }
 
@@ -24,7 +26,34 @@ namespace OrderProcessor.Consumers
             _logger.LogInformation($"Received `OrderCollected` event with content: {message}");
 
             // Out
-            var result = await _grpcOrderClient.UpdateOrder(orderCollected.OrderId, orderStatus: Common.Models.OrderStatus.Completed);
+            await _grpcOrderClient.UpdateOrder(orderCollected.OrderId, orderStatus: Common.Models.OrderStatus.Completed);
+
+            // AddUserPromo
+            var order = await _grpcOrderClient.GetOrder(orderCollected.OrderId);
+            var promoValidUntil = DateTime.Now + TimeSpan.FromDays(30);
+
+            var addUserPromo = new AddUserPromo
+            {
+                CustomerInfo = order.CustomerInfo,
+                Promo = GetPromoForUser(order.TotalPrice),
+                ValidUntil = promoValidUntil
+            };
+
+            await _publishEndpoint.Publish(addUserPromo);
+
+            message = JsonSerializer.Serialize(addUserPromo);
+            _logger.LogInformation($"Sent `AddUserPromo` event with content: {message}");
+        }
+
+        // To be moved to Discount microservice
+        public static double GetPromoForUser(double totalPrice)
+        {
+            if (totalPrice < 50.0)
+                return 0.05;
+            if (totalPrice < 100.0)
+                return 0.1;
+
+            return 0.15;
         }
     }
 }
