dbe64e7f6ffd0a83cc5486c6b703e8cbd150f9d4
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2023-08-22 20:17:45 +0300

    Implement basic Overview page.

--- WebUI/Pages/MyAccount/Overview.cshtml.cs
+++ WebUI/Pages/MyAccount/Overview.cshtml.cs
@@ -1,11 +1,31 @@
+using Common.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebUI.Services;
+using WebUI.Users;
 
 namespace WebUI.Pages.MyAccount
 {
     public class Overview : PageModel
     {
-        public void OnGet()
+        private readonly ICustomerService _customerService;
+        private readonly IUserProvider _userProvider;
+
+        [BindProperty]
+        public Customer Customer { get; set; }
+
+        public Overview(ICustomerService customerService, IUserProvider userProvider)
         {
+            _customerService = customerService;
+            _userProvider = userProvider;
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var customerId = _userProvider.GetCustomerId(HttpContext);
+            Customer = await _customerService.GetCustomerById(customerId);
+
+            return Page();
         }
     }
 }
