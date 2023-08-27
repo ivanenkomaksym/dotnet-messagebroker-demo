8a6f628224c85736f60ed11db64839059d188a18
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2023-08-27 14:17:57 +0300

    Add basic Discounts page.

---
+++ WebUI/Pages/MyAccount/Discounts.cshtml.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebUI.Models.Discounts;
+using WebUI.Services;
+using WebUI.Users;
+
+namespace WebUI.Pages.MyAccount
+{
+    public class DiscountsModel : PageModel
+    {
+        private readonly IDiscountService _discountService;
+        private readonly IUserProvider _userProvider;
+
+        [BindProperty]
+        public UserPromo UserPromo { get; set; }
+
+        public DiscountsModel(IDiscountService discountService, IUserProvider userProvider)
+        {
+            _discountService = discountService;
+            _userProvider = userProvider;
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var customerId = _userProvider.GetCustomerId(HttpContext);
+            UserPromo = await _discountService.GetUserPromoForCustomerById(customerId);
+
+            return Page();
+        }
+    }
+}
