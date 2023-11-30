dfd02edb032398b913468d5a95efecfa451f0dde
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2023-11-30 19:04:06 +0200

    Add missing Instrumentation file.

---
+++ Common/Extensions/Instrumentation.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.Metrics;
+using System.Diagnostics;
+
+namespace Common.Extensions
+{
+    /// <summary>
+    /// It is recommended to use a custom type to hold references for
+    /// ActivitySource and Instruments. This avoids possible type collisions
+    /// with other components in the DI container.
+    /// </summary>
+    public class Instrumentation : IDisposable
+    {
+        internal const string ActivitySourceName = "EShopActivity";
+        internal const string MeterName = "EShopActivity";
+        private readonly Meter meter;
+
+        public Instrumentation()
+        {
+            string? version = typeof(Instrumentation).Assembly.GetName().Version?.ToString();
+            this.ActivitySource = new ActivitySource(ActivitySourceName, version);
+            this.meter = new Meter(MeterName, version);
+        }
+
+        public ActivitySource ActivitySource { get; }
+
+        public void Dispose()
+        {
+            this.ActivitySource.Dispose();
+            this.meter.Dispose();
+        }
+    }
+}
