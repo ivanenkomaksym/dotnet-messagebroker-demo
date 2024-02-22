63c551f77448288705fb1a4ab23298f5252bd431
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2024-02-22 09:59:08 +0200

    Use latest tag. Add webui with stub services yaml. Add script to start minimal k8s env with webui with stubs.

---
+++ 04_run_minimal_k8s.ps1
@@ -0,0 +1,61 @@
+cd charts\k8s
+.\cleanup.ps1
+
+# RabbitMQ
+kubectl apply -f rabbitmq.yaml
+kubectl apply -f rabbitmq-service.yaml
+
+# Mongo
+kubectl apply -f mongo-secret.yaml
+kubectl apply -f mongo-deployment.yaml
+kubectl apply -f mongo-service.yaml
+
+# CatalogAPI Stub
+
+# CustomerAPI Stub
+
+# Notifications
+
+# OcelotApiGateway
+kubectl apply -f ocelot-configmap.yaml
+kubectl apply -f ocelotapigateway.yaml
+kubectl apply -f ocelotapigateway-service.yaml
+
+# OrderAPI
+kubectl apply -f orderapi.yaml
+kubectl apply -f orderapi-service.yaml
+
+# OrderGRPC
+kubectl apply -f ordergrpc.yaml
+kubectl apply -f ordergrpc-service.yaml
+
+# OrderProcessor
+kubectl apply -f orderprocessor.yaml
+
+# Payment
+kubectl apply -f payment.yaml
+
+# Shipment
+kubectl apply -f shipment.yaml
+
+# ShoppingCartAPI Stub
+
+# Warehouse
+kubectl apply -f warehouse.yaml
+
+# WarehouseAPI Stub
+
+# WebUI
+kubectl apply -f webui_with_stubs.yaml
+kubectl apply -f webui-service.yaml
+
+# WebUIAggregatorAPI Stub
+
+# FeedbackAPI Empty
+
+# DiscountAPI Empty
+
+minikube dashboard
+Start-Process -FilePath "cmd" -ArgumentList "/c minikube service shopdb" -WorkingDirectory ".\"
+Start-Process -FilePath "cmd" -ArgumentList "/c minikube service webui" -WorkingDirectory ".\"
+cd ..\..\
