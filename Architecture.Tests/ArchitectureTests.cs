abae3f0ea18f8f65998829bd245baabcfa57c4be
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2024-04-12 10:10:41 +0300

    Add first two fitness functions. 1. Common types sould be public. 2. Interfaces should be public and start with I.

--- Architecture.Tests/ArchitectureTests.cs
+++ Architecture.Tests/ArchitectureTests.cs
@@ -1,11 +1,68 @@
+using System.Reflection;
+using Architecture.Tests.Rules;
+using Common.Configuration;
+using FluentAssertions;
+using NetArchTest.Rules;
+
 namespace Architecture.Tests
 {
     public class ArchitectureTests
     {
         [Fact]
-        public void Test1()
+        public void GivenCommonTypes_WhenAccessedFromOtherProjects_ThenTheyAreVisible()
         {
+            // Arrange
+            var commonAssembly = typeof(ApiSettings).Assembly;
 
+            // Act
+            var result = Types.InAssembly(commonAssembly)
+                .Should().BePublic()
+                .GetResult();
+
+            // Assert
+            result.IsSuccessful.Should().BeTrue();
+        }
+
+        [Fact]
+        public void GivenServiceInterfaces_ThenShouldBePublicAndBeInterfacesAndStartWithI()
+        {
+            // Arrange
+            var interfacesShouldStartWithIRule = new InterfacesShouldStartWithIRule();
+
+            foreach (var assembly in GetAllAssemblies())
+            {
+                // Act
+                var result = Types.InAssembly(assembly)
+                    .Should().MeetCustomRule(interfacesShouldStartWithIRule)
+                    .GetResult();
+
+                // Assert
+                result.IsSuccessful.Should().BeTrue();
+            }
+        }
+
+        private IEnumerable<Assembly> GetAllAssemblies()
+        {
+            return new[]
+            {
+                typeof(Catalog.API.Controllers.CatalogController).Assembly,
+                typeof(ApiSettings).Assembly,
+                typeof(CustomerAPI.Controllers.CustomerController).Assembly,
+                typeof(FeedbackAPI.Data.IFeedbackContext).Assembly,
+                typeof(Notifications.NotificationsWorker).Assembly,
+                typeof(OrderAPI.Controllers.OrderController).Assembly,
+                typeof(OrderCommon.Data.IOrderContext).Assembly,
+                typeof(OrderGrpc.Worker).Assembly,
+                typeof(OrderProcessor.OrderProcessorWorker).Assembly,
+                typeof(PaymentService.PaymentWorker).Assembly,
+                typeof(Shipment.Worker).Assembly,
+                typeof(ShoppingCartAPI.Controllers.ShoppingCartController).Assembly,
+                typeof(Warehouse.WarehouseWorker).Assembly,
+                typeof(WarehouseAPI.Controllers.StockItemController).Assembly,
+                typeof(WarehouseCommon.Data.IWarehouseContext).Assembly,
+                typeof(WebUI.Services.ICustomerService).Assembly,
+                typeof(WebUIAggregatorAPI.Controllers.ProductsController).Assembly
+            };
         }
     }
 }
---
+++ Architecture.Tests/Rules/InterfacesShouldStartWithIRule.cs
@@ -0,0 +1,18 @@
+﻿using NetArchTest.Rules;
+
+namespace Architecture.Tests.Rules
+{
+    public class InterfacesShouldStartWithIRule : ICustomRule
+    {
+        public bool MeetsRule(Mono.Cecil.TypeDefinition type)
+        { 
+            if (!type.IsInterface)
+                return true;
+
+            if (!type.Name.StartsWith('I'))
+                return false;
+
+            return true;
+        }
+    }
+}
