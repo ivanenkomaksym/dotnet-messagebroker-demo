fe57bec2288c6b08893c672b204b43b110dac571
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2024-09-08 12:16:18 +0300

    Include Customer, Catalog, WarehouseAPI and ShoppingCartAPI in Aspire with feature flags enabled.

--- eshop-aspire.AppHost/Program.cs
+++ eshop-aspire.AppHost/Program.cs
@@ -1,37 +1,53 @@
+using Microsoft.Extensions.Configuration;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+var featureFlagsOptions = builder.Configuration.GetSection("FeatureManagement");
+var useCustomer = featureFlagsOptions.GetValue<bool>("Customer");
+var useProduct = featureFlagsOptions.GetValue<bool>("Product");
+var useShoppingCart = featureFlagsOptions.GetValue<bool>("ShoppingCart");
 
 var mongo = builder.AddMongoDB("mongo").PublishAsConnectionString();
 var mongodb = mongo.AddDatabase("mongodb");
 
 var messaging = builder.AddRabbitMQ("AMQPConnectionString");
 
-var customers = builder.AddProject<Projects.CustomerAPI>("customerapi")
-    .WithReference(mongodb)
-    .WithReference(messaging)
-    // Use Aspire's mongodb connection string in CustomerAPI's appsettings
-    .WithEnvironment($"DatabaseSettings:ConnectionString", mongodb)
-    .WithEnvironment($"ApplicationOptions:StartupEnvironment", "Aspire");
+if (useCustomer)
+{
+    var customers = builder.AddProject<Projects.CustomerAPI>("customerapi")
+        .WithReference(mongodb)
+        .WithReference(messaging)
+        // Use Aspire's mongodb connection string in CustomerAPI's appsettings
+        .WithEnvironment($"DatabaseSettings:ConnectionString", mongodb)
+        .WithEnvironment($"ApplicationOptions:StartupEnvironment", "Aspire");
+}
 
-var catalog = builder.AddProject<Projects.CatalogAPI>("catalogapi")
-    .WithReference(mongodb)
-    // Use Aspire's mongodb connection string in CatalogAPI's appsettings
-    .WithEnvironment($"DatabaseSettings:ConnectionString", mongodb);
+if (useProduct)
+{
+    var catalog = builder.AddProject<Projects.CatalogAPI>("catalogapi")
+        .WithReference(mongodb)
+        // Use Aspire's mongodb connection string in CatalogAPI's appsettings
+        .WithEnvironment($"DatabaseSettings:ConnectionString", mongodb);
 
-var shoppingCart = builder.AddProject<Projects.ShoppingCartAPI>("shoppingcartapi")
-    .WithReference(mongodb)
-    // Use Aspire's mongodb connection string in ShoppingCartAPI's appsettings
-    .WithEnvironment($"DatabaseSettings:ConnectionString", mongodb);
+    var warehouseapi = builder.AddProject<Projects.WarehouseAPI>("warehouseapi")
+        .WithReference(mongodb)
+        // Use Aspire's mongodb connection string in WarehouseAPI's appsettings
+        .WithEnvironment($"DatabaseSettings:ConnectionString", mongodb);
 
-var warehouseapi = builder.AddProject<Projects.WarehouseAPI>("warehouseapi")
-    .WithReference(mongodb)
-    // Use Aspire's mongodb connection string in WarehouseAPI's appsettings
-    .WithEnvironment($"DatabaseSettings:ConnectionString", mongodb);
+    // Configure WebUIAggregator to startup in Aspire configuration, since it uses two services: CatalogAPI and WarehouseAPI to aggregate response
+    var webuiaggregator = builder.AddProject<Projects.WebUIAggregatorAPI>("webuiaggregatorapi")
+        .WithReference(catalog)
+        .WithReference(warehouseapi)
+        .WithEnvironment($"ApplicationOptions:StartupEnvironment", "Aspire");
+}
 
-// Configure WebUIAggregator to startup in Aspire configuration, since it uses two services: CatalogAPI and WarehouseAPI to aggregate response
-var webuiaggregator = builder.AddProject<Projects.WebUIAggregatorAPI>("webuiaggregatorapi")
-    .WithReference(catalog)
-    .WithReference(warehouseapi)
-    .WithEnvironment($"ApplicationOptions:StartupEnvironment", "Aspire");
+if (useShoppingCart)
+{
+    var shoppingCart = builder.AddProject<Projects.ShoppingCartAPI>("shoppingcartapi")
+        .WithReference(mongodb)
+        // Use Aspire's mongodb connection string in ShoppingCartAPI's appsettings
+        .WithEnvironment($"DatabaseSettings:ConnectionString", mongodb);
+}
 
 var order = builder.AddProject<Projects.OrderAPI>("orderapi")
     .WithReference(mongodb)
@@ -77,17 +93,13 @@ var warehouse = builder.AddProject<Projects.Warehouse>("warehouse")
 builder.AddProject<Projects.WebUI>("webui")
     .WithExternalHttpEndpoints()
     .WithReference(messaging)
-    .WithReference(customers)
-    .WithReference(catalog)
-    .WithReference(shoppingCart)
-    .WithReference(webuiaggregator)
     .WithReference(order)
     // Configure WebUI to use real CustomerAPI
-    .WithEnvironment($"FeatureManagement:Customer", "true")
+    .WithEnvironment($"FeatureManagement:Customer", useCustomer.ToString())
     // Configure WebUI to use real CatalogAPI
-    .WithEnvironment($"FeatureManagement:Product", "true")
+    .WithEnvironment($"FeatureManagement:Product", useProduct.ToString())
     // Configure WebUI to use real ShoppingCartAPI
-    .WithEnvironment($"FeatureManagement:ShoppingCart", "true")
+    .WithEnvironment($"FeatureManagement:ShoppingCart", useShoppingCart.ToString())
     .WithEnvironment($"ApplicationOptions:StartupEnvironment", "Aspire");
 
 builder.Build().Run();
