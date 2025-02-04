9dfc1789644ac89ac65a8ef65c8bf947aee68e11
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2025-02-04 16:48:43 +0200

    Add first version of dotnet format script. It will format only git changed files.

---
+++ format_changed_files.ps1
@@ -0,0 +1,6 @@
+﻿$files = git diff --name-only --diff-filter=ACMRT | Where-Object { $_ -match '\.cs$' }
+if ($files.Count -gt 0) {
+    dotnet format --include @files -v diagnostic
+} else {
+    Write-Host "No C# files to format."
+}
