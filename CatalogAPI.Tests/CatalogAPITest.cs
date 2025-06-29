92b89fa7d5a89c2155247b2d5b8a27c3a9fe80e9
ivanenkomaksym <ivanenkomaxim@yahoo.com>
2025-06-29 10:02:31 +0300

    Add CatalogAPI.Tests project based on Testcontainers.

---
+++ CatalogAPI.Tests/CatalogAPITest.cs
@@ -0,0 +1,42 @@
+﻿using CatalogAPI.Data;
+using Common.Configuration;
+using Microsoft.Extensions.Options;
+using Testcontainers.MongoDb;
+
+namespace CatalogAPI.Tests;
+
+public class CatalogAPITest : IAsyncLifetime
+{
+    private readonly MongoDbContainer _mongoDbContainer = new MongoDbBuilder()
+        .WithImage("mongo:latest")
+        .Build();
+
+    public Task InitializeAsync()
+    {
+        return _mongoDbContainer.StartAsync();
+    }
+
+    public Task DisposeAsync()
+    {
+        return _mongoDbContainer.DisposeAsync().AsTask();
+    }
+
+    [Fact]
+    public void ShouldReturnProducts()
+    {
+        // Arrange
+        var settings = Options.Create(new DatabaseSettings
+        {
+            ConnectionString = _mongoDbContainer.GetConnectionString(),
+            DatabaseName = "CatalogDb",
+            CollectionName = "Products"
+        });
+        var catalogContext = new CatalogContext(settings);
+
+        // Act
+        var products = catalogContext.Products;
+
+        // Assert
+        Assert.NotNull(products);
+    }
+}
